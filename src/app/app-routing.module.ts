963267f2b97c6384f12032c8c89811deabf09496
Osama-Mamdoh <osama.mamdoh.7@gmail.com>
2021-01-25 20:21:37 +0200

    init project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, ExtraOptions } from '@angular/router';
+import { AuthGuard, NoAuthGuard } from '@core/guards';
 
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./modules/budgets/budgets.module').then((m) => m.BudgetsModule),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./modules/auth/auth.module').then((m) => m.AuthModule),
+    canActivate: [NoAuthGuard],
+  },
+  { path: '**', redirectTo: '' },
+];
 
-const routes: Routes = [];
-
+const routerOptions: ExtraOptions = {
+  useHash: false,
+};
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, routerOptions)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
